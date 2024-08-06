dfc94f5abdb72b3fc7caac8724a1418af71c066f
tsars <tsarskiytsarskiy@mail/ru>
2024-08-06 13:57:16 +0700

    готово

--- mysite/task1/models.py
+++ mysite/task1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
-    balance = models.DecimalField(max_length=100, decimal_places=100, max_digits=100)
+    balance = models.DecimalField(max_length=100, decimal_places=2, max_digits=100)
     age = models.IntegerField()
 
     def __str__(self):
@@ -12,7 +12,8 @@ class Buyer(models.Model):
 class Game(models.Model):
     title = models.CharField(max_length=100)
     cost = models.IntegerField()
-    description = models.TimeField()
+    description = models.CharField(max_length=1000)
+    size = models.DecimalField(decimal_places=2, max_digits=100)
     age_limited = models.BooleanField()
     buyer = models.ManyToManyField(Buyer, related_name='правобладатель')
 
