a62ead21de696b0327ace753c539f8fec0a6f971
tsars <tsarskiytsarskiy@mail/ru>
2024-08-06 16:43:29 +0700

    готово1

--- mysite/task1/models.py
+++ mysite/task1/models.py
@@ -15,9 +15,9 @@ class Game(models.Model):
     description = models.CharField(max_length=1000)
     size = models.DecimalField(decimal_places=2, max_digits=100)
     age_limited = models.BooleanField()
-    buyer = models.ManyToManyField(Buyer, related_name='правобладатель')
+    buyer = models.ManyToManyField(Buyer)
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
