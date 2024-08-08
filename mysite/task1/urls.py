9f69253cb66844e0efc7ddc7660bb307860151ca
tsars <tsarskiytsarskiy@mail.ru>
2024-08-08 16:56:03 +0700

    111

---
+++ mysite/task1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('registration/platform/', views.index, name='index'),
+    path('registration/platform/gemes/', views.index2, name='index2'),
+    path('registration/platform/cart/', views.index3, name='index3'),
+    path('registration/', views.index4, name='index4'),
+]
+
